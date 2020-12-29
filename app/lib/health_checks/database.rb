7e65e94eaf450203923cc3cd407f365baa0323c4
Israel Tomilayo <itomilayo@gmail.com>
2020-12-29 17:02:47 +0100

    create healthcheck library module to monitor database availability

---
+++ app/lib/health_checks/database.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module HealthChecks
+  class Database
+    include Helper
+
+    def check
+      [
+        run_check('db.api.connection') do
+          ActiveRecord::Base.connected?
+        end
+      ]
+    end
+
+    def self.all_pass?(results)
+      results.all? { |result| result[:pass] == true }
+    end
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module NewsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.paths.add 'app/lib', eager_load: true, glob: '**/*.rb'
   end
 end
