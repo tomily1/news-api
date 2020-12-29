4bc94361a51fae1cc78f352a6fdda5f6a9422db1
Israel Tomilayo <itomilayo@gmail.com>
2020-12-29 17:07:31 +0100

    add healthcheck and heartbeat route to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :heartbeat, controller: :heartbeat
+  resource :healthcheck, controller: :health_check
 end
