b1ca5a83fc7fb134987c021c1b2b1c1df3825927
Israel Tomilayo <itomilayo@gmail.com>
2020-12-29 21:55:21 +0100

    create basic routes for user management

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :heartbeat, controller: :heartbeat
   resource :healthcheck, controller: :health_check
+
+  post '/login', to: 'authentication#login'
+  post '/logout', to: 'authentication#logout'
+
+  resource :user, only: :create
 end
