81f5f9a16b119a99b9e65299ab3abdfb226d550e
Israel Tomilayo <itomilayo@gmail.com>
2020-12-30 18:26:14 +0100

    seed the database with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+AdminUser.create(email: 'test@camillion.app', password: 'password', super_admin: true)
+User.create(email: 'test@camillion.app', password: 'password')
+
+5.times do
+  Post.create(
+    title: 'mock title',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!'
+  )
+end
