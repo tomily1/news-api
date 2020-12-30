b84be1308dcb61c7ca2c231c6ffb9d98a14e0268
Israel Tomilayo <itomilayo@gmail.com>
2020-12-30 13:20:56 +0100

    make admin controller lean

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,30 +1,7 @@
 # frozen_string_literal: true
 
 class AdminsController < ApplicationController
-  before_action :authorize_admin, except: %i[login]
-  before_action :authorize_super_admin, only: :create
-
-  def login
-    authenticate_user(AdminUser, params)
-  end
-
-  def create
-    register_user(AdminUser, admin_params)
-  end
-
   private
-
-  def admin_params
-    params.permit(
-      :email,
-      :password,
-      :password_confirmation
-    )
-  end
-
-  def authorize_super_admin
-    return unauthorized unless current_admin&.super_admin?
-  end
 
   def authorize_admin
     return unauthorized('No authentication token provided') unless header =~ AUTH_FORMAT
