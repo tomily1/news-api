8710f548837b20785dc0e5ee03a0dc3f9cd53b22
Israel Tomilayo <itomilayo@gmail.com>
2020-12-30 13:09:04 +0100

    create news table to add news post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_095302) do
+ActiveRecord::Schema.define(version: 2020_12_30_112312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2020_12_30_095302) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email"
+  end
+
+  create_table "news", force: :cascade do |t|
+    t.text "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "token_blacklists", force: :cascade do |t|
